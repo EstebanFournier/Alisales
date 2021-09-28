9a07debdf7609daac4422b1d914d7da6ceb1ba49
EstebanFournier <77848269+EstebanFournier@users.noreply.github.com>
2021-09-28 10:32:53 +0200

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\CustomerController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect(route('login'));
+});
+
+Auth::routes();
+
+Route::middleware('auth')->group(function(){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('customer', CustomerController::class);
+    Route::get('customer/{customer}/delete',[CustomerController::class,'delete'])->name('customer.delete');
+    Route::get('customer/{customer}/createOrder',[CustomerController::class,'createOrder'])->name('customer.createOrder');
+    Route::post('customer/{customer}/storeOrder',[CustomerController::class,'storeOrder'])->name('customer.storeOrder');
+
+
+    Route::resource('order',\App\Http\Controllers\OrderController::class);
+    Route::get('order/{order}/delete',[\App\Http\Controllers\OrderController::class,'delete'])->name('order.delete');
+
+});
